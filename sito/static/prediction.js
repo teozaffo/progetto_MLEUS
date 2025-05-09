29266bd193ce8f638c782a32bb5c38e22d0e02b6
je <john.eric.arriola@gmail.com>
2025-05-09 16:32:22 +0200

    Aggiunto il listener per il submit del form

--- sito/static/prediction.js
+++ sito/static/prediction.js
@@ -5,3 +5,41 @@ window.onload = () => {
   document.getElementById("result").style.backgroundColor = `${localStorage.getItem("predictionBackgroundColor")}`;
   document.getElementById("result").style.color = `${localStorage.getItem("predictionColor")}`;
 }
+
+document,addEventListener('DOMContentLoaded', () => {
+  document
+    .getElementById("feedback")
+    .addEventListener(
+      "submit",
+      (event) => submitFeedbackForm(event)
+    );
+})
+
+const submitFeedbackForm = (event) => {
+  event.preventDefault();
+
+  const reliability = document
+    .querySelector('input[name="reliability"]:checked')?.value;
+  
+  const usefulness = document
+    .querySelector('input[name="usefulness"]:checked')?.value;
+
+  const influence = document
+    .querySelector('input[name="influence"]:checked')?.value;
+
+  const recommendability = document
+    .getElementById('recommendability').value;
+
+  const reason = document
+    .getElementById('reason').value;
+
+  const feedback = {
+    reliability,
+    usefulness,
+    influence,
+    recommendability,
+    reason
+  };
+
+  console.log(feedback);
+}
