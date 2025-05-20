7a60d9a19313af39f45951f0af52aacf716b6f93
teozaffo <126102609+teozaffo@users.noreply.github.com>
2025-05-20 15:54:14 +0200

    inserite scritte variabili in base alla percentuale

--- sito/static/prediction.js
+++ sito/static/prediction.js
@@ -1,12 +1,48 @@
 window.onload = () => {
   // Popola il risultato nella pagina
+
+  // Modifica la risposta in base alla percentuale
+	const rawPrediction = localStorage.getItem("prediction") || ":: Nessun risultato ::";
+
+    const match = rawPrediction.match(/(\d+)%/);  // cattura "23%" ovunque
+    let displayPrediction = ":: Invalid prediction format ::";
+
+    if (match) {
+      const percent = parseInt(match[1]);
+
+      if (percent < 5) {
+        displayPrediction = "Most Likely Benign";
+      } else if (percent < 45) {
+        displayPrediction = "Likely Benign";
+      } else if (percent <= 55) {
+        displayPrediction = "Uncertain Case";
+      } else if (percent <= 95) {
+        displayPrediction = "Likely Malign";
+      } else {
+        displayPrediction = "Most Likely Malignant";
+      }
+    } else {
+      displayPrediction = rawPrediction.replace(/\d+% /, "");  // fallback se non è presente la percentuale
+    }
+
+    document.getElementById("result").innerText = displayPrediction;
   
   // Nasconde la percentuale
-  const rawPrediction = localStorage.getItem("prediction") || ":: Nessun risultato ::";
+  /*const rawPrediction = localStorage.getItem("prediction") || ":: Nessun risultato ::";
   const cleanedPrediction = rawPrediction.replace(/\d+% /, "");  // rimuove "23% "
-  document.getElementById("result").innerText = cleanedPrediction;
+  document.getElementById("result").innerText = cleanedPrediction;*/
   
+  // Randomizza la visualizzazione della percentuale
+  /*const rawPrediction = localStorage.getItem("prediction") || ":: Nessun risultato ::";
+  const showPercentage = Math.random() < 0.5;
+  const displayPrediction = showPercentage
+    ? rawPrediction
+    : rawPrediction.replace(/\d+% /, "");
+  document.getElementById("result").innerText = displayPrediction;*/
+  
+  //Mostra la percentuale
   // document.getElementById("result").innerText = localStorage.getItem("prediction") || ":: Nessun risultato ::";
+  
   document.getElementById("result").style.backgroundColor = localStorage.getItem("predictionBackgroundColor") || "#f0f0f0";
   document.getElementById("result").style.color = localStorage.getItem("predictionColor") || "black";
   document.getElementById("selected-model").innerText = `Selected Model: ${localStorage.getItem("model") || "-"}`;
